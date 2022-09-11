7eb34a5bc5c056caa1a3e60f415ac24cd0a89c16
Elton Mziti <eltonmziti94@gmail.com>
2022-09-10 19:57:14 -0700

    Task 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
-#include <studio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
 /**
- * main - entry point
- *
- * Return: 0 after printing the function 
- */
+ * * main - print the string in the put function
+ * *
+ * * Description: using the main function
+ * * this program prints "Programming is like building a multilingual puzzle"
+ * * Return: 0
+ * */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-       return (0);
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
 }
 
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: 0 (Success)
- */
+ * * main - print the string  in the put function
+ * *
+ * * Description: using the main function
+ * * this program prints "Programming is like building a multilingual puzzle"
+ * * Return: 0
+ * */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return(0);
-}	
+    	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
+
