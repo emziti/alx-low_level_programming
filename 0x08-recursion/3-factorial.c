195d7fc7dcd8723ee7ccae2638ca9938a1b2504c
Elton Mziti <eltonmziti94@gmail.com>
2022-09-28 07:01:10 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,28 @@
-#include "main.h"
-/**
- * factorial - returns factorial of a given number.
- * @n: number to find factorial of.
- *
- * Return: factorial of number 
- *-1 if n < 0.
- */
-int factorial(n < 0)
-{
-	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (1);
-	return (n * factorial (n - 1));
+/*                                                                                                                                      
+ * 3-factorial.c                                                                                                                        
+ * Auth: sam                                                                                                                            
+ */                                                                                                                                     
+                                                                                                                                        
+#include "main.h"                                                                                                                       
+                                                                                                                                        
+/**                                                                                                                                     
+ * factorial - Returns the factorial of a given number.                                                                                 
+ * @n: The number to find the factorial of.                                                                                             
+ *                                                                                                                                      
+ * Return: If n > 0 - the factorial of n.                                                                                               
+ *         If n < 0 - 1 to indicate an error.                                                                                           
+ */                                                                                                                                     
+int factorial(int n)                                                                                                                    
+{                                                                                                                                       
+        int result = n;                                                                                                                 
+                                                                                                                                        
+        if (n < 0)                                                                                                                      
+                return (-1);                                                                                                            
+                                                                                                                                        
+        else if (n >= 0 && n <= 1)                                                                                                      
+                return (1);                                                                                                             
+                                                                                                                                        
+        result *= factorial(n - 1);                                                                                                     
+                                                                                                                                        
+        return (result);                                                                                                                
 }
-
